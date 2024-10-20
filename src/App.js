447af3616564541cdf2c6194f5c40181a2cac904
Jay Savaliya <jrsavaliya04@gmail.com>
2024-10-20 18:27:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Auth from './components/Auth';
+
+function App() {
+  return (
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+      <Auth />
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/services/axiosInstance.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
+// Axios Instance
+const instance = axios.create({
+  baseURL: import.meta.env.NODE_APP_SERVER_URL
+});
+
+// Request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token != null) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (axios.isCancel(error)) {
+      // console.log('canceled');
+    } else if (!error.response) {
+      toast.error("Network error");
+      // localStorage.removeItem('role');
+      // localStorage.removeItem('token');
+      // localStorage.removeItem('userId');
+    }
+    if (error?.response?.status === 401) {
+      localStorage.removeItem("role");
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      setTimeout(() => {
+        window.location.reload();
+      }, 5000);
+    } else {
+      // toast.error(error.response.data.message);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
